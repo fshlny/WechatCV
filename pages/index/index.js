e9d08c9cc96ecd45773c15887c358f9313af5bc3
fshlny <467594943@qq.com>
2018-05-05 21:25:45 +0800

    修改index页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,21 +8,6 @@ Page({
     address:'上海市xxx区xxx街xx号'
   },
   onLoad: function () {
-    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
-    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
-  },
-  toMainPage:()=>{
-    console.log(`当前页面的URL是:${getCurrentPages()}`, getCurrentPages());
-    wx.navigateTo({
-      url: '../main/main',
-    })
-  },
-  onLoad: function (options) {
-    let timeOut = setTimeout(()=>{
-      wx.navigateTo({
-        url: '../main/main',
-      });
-      clearTimeout(timeOut);
-    },1000);
+   
   }
 })
